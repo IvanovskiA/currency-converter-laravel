5fb21da259f2a55a686a50feb25c187a44616fa8
IvanovskiA <ivanovskiivanovski5@gmail.com>
2025-09-18 16:28:23 +0200

    add conversions table migration

---
+++ database/migrations/2025_09_18_135514_create_conversions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conversions', function (Blueprint $table) {
+            $table->id();
+            $table->string('from_currency', 3);
+            $table->string('to_currency', 3);
+            $table->decimal('amount', 15, 2);
+            $table->decimal('rate', 15, 6);
+            $table->decimal('result', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conversions');
+    }
+};
